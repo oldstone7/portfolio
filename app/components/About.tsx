5460b65292e6bc1ed38a6ae9a01ef05cee6181d8
oldstone7 <sulaimanalfareeth@gmail.com>
2025-08-28 22:14:59 +0530

    Aboutme change

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -3,12 +3,12 @@ export default function About() {
     <section id="about" className="py-16 bg-white dark:bg-gray-900">
       <div className="container mx-auto px-6">
         <h2 className="text-3xl font-bold text-center text-gray-800 dark:text-white mb-8">About Me</h2>
-        <p className="text-lg text-gray-600 dark:text-gray-300 max-w-3xl mx-auto text-center mb-8">
-          Hi, I'm a fullstack web developer with an interest for creating efficient,
-          scalable, and user-friendly web applications with expertise in responsive frontend design, backend development
-           with RESTful APIs, and databases. Ready to learn new technologies and adapt to project needs, striving
-            to deliver impactful solutions.
-        </p>
+        <div className="text-lg text-gray-600 dark:text-gray-300 max-w-3xl mx-auto text-center mb-8">
+          <p className="font-bold text-center">What do I do? I develop.</p>
+          <p className="mt-4">
+            Tools, microservices, frontends, backends, APIs, full-stack web apps, mobile apps—you name it.
+          </p>
+        </div>
         <div className="flex justify-center mt-6">
           <a 
             href="/Sulaiman_Alfareeth.pdf" 
